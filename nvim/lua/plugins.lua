0fe05c747719b211bec27393494ad9fd0882dad4
Mentalina <onextthenitromancer@gmail.com>
2025-04-25 12:23:56 +0200

    Coc keybindings

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -84,13 +84,23 @@ require("nvim-surround").setup()
 require("nvim-tree").setup()
 
 --CoC
-vim.opt.backup = false
-vim.opt.writebackup = false
-vim.opt.updatetime = 300
--- Highlight the symbol and its references on a CursorHold event(cursor is idle)
-vim.api.nvim_create_augroup("CocGroup", {})
-vim.api.nvim_create_autocmd("CursorHold", {
-    group = "CocGroup",
-    command = "silent call CocActionAsync('highlight')",
-    desc = "Highlight symbol under cursor on CursorHold"
-})
+	vim.opt.backup = false
+	vim.opt.writebackup = false
+	vim.opt.updatetime = 300
+	-- Highlight the symbol and its references on a CursorHold event(cursor is idle)
+	vim.api.nvim_create_augroup("CocGroup", {})
+	vim.api.nvim_create_autocmd("CursorHold", {
+		group = "CocGroup",
+		command = "silent call CocActionAsync('highlight')",
+		desc = "Highlight symbol under cursor on CursorHold"
+	})
+	-- Complete on enter
+	vim.keymap.set("i", "<CR>", [[coc#pum#visible() ? coc#pum#confirm() : "\<CR>"]], {expr = true})
+	-- Cycle through completion list with tab
+	vim.keymap.set("i", "<Tab>", [[coc#pum#visible() ? coc#pum#next(1) : "\<Tab>"]], {expr = true})
+	vim.keymap.set("i", "<S-Tab>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<S-Tab>"]], {expr = true})
+	-- Go to definition (Ctl-o to go back)
+	vim.keymap.set("n", "gd", "<Plug>(coc-definition)", {silent = true})
+	vim.keymap.set("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
+	vim.keymap.set("n", "gi", "<Plug>(coc-implementation)", {silent = true})
+	vim.keymap.set("n", "gr", "<Plug>(coc-references)", {silent = true})
